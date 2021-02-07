1decab017cf45e76b33b0682c4033e9d35eb0ead
James Dingle <ding_jimmy@hotmail.com>
2021-02-07 23:30:08 +0000

    init action class

---
+++ src/Springhare/Action.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) James C Dingle. All rights reserved.
+
+using System;
+
+namespace Springhare
+{
+    public class Action
+    {
+    }
+}
--- src/Springhare/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Springhare
-{
-    public class Class1
-    {
-    }
-}
