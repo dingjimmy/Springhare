5a08190ca9084ea604f2fc4bcc23e28cde30c81e
James Dingle <ding_jimmy@hotmail.com>
2021-02-09 21:15:25 +0000

    Generic actions

--- src/Springhare/IAction.cs
+++ src/Springhare/IAction.cs
@@ -7,46 +7,37 @@ namespace Springhare
     /// <summary>
     /// Represents an action that need to be performed by a 3rd party device, application or service.
     /// </summary>
-    public class Action
+    public interface IAction
     {
         ///<Summary>
         /// Gets or sets a value which uniquley identifies the action.
         ///</Summary>
-        public uint Id { get; set; }
+        uint Id { get; set; }
 
         /// <summary>
         /// Gets or sets human-friendly name to help users to identify the Action.
         /// </summary>
-        public string Name { get; set; }
+        string Name { get; set; }
 
         // <summary>
         /// Gets a collection of paramters that configure the actions behaviour.
         /// </summary>
-        public List<ConfigurationParameter> Configuration { get; set; }
+        List<ConfigurationParameter> Configuration { get; set; }
 
         /// <summary>
         /// Sets-up the action ready for execution. 
         /// </summary>
-        public void Setup()
-        {
-
-        }
+        public void Setup();
 
         /// <summary>
         /// Executes the primary logic of the action.
         /// </summary>
-        public void Execute()
-        {
-
-        }
+        public void Execute();
 
         /// <summary>
         /// Tears-down the action after execution has completed.
         /// </summary>
         /// <remarks>For Example: freeing resources, closing connections and putting 3rd party devices, software or services into a good state.</remarks>
-        public void Teardown()
-        {
-
-        }
+        public void Teardown();
     }
 }
---
+++ src/Springhare/LongRunningAction.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) James C Dingle. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+
+namespace Springhare
+{
+    public class LongRunningAction : IAction
+    {
+        public uint Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<ConfigurationParameter> Configuration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Setup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Teardown()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
