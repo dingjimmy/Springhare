fc0a05ee98c91432c1d48b9f47976b84631c5ca8
James Dingle <ding_jimmy@hotmail.com>
2021-02-09 21:05:56 +0000

    Actions

--- src/Springhare/Action.cs
+++ src/Springhare/Action.cs
@@ -1,10 +1,52 @@
-﻿// Copyright (c) James C Dingle. All rights reserved.
+﻿// Copyright (c) James C Dingle. All rights reserved.:warning
 
-using System;
+using System.Collections.Generic;
 
 namespace Springhare
 {
+    /// <summary>
+    /// Represents an action that need to be performed by a 3rd party device, application or service.
+    /// </summary>
     public class Action
     {
+        ///<Summary>
+        /// Gets or sets a value which uniquley identifies the action.
+        ///</Summary>
+        public uint Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets human-friendly name to help users to identify the Action.
+        /// </summary>
+        public string Name { get; set; }
+
+        // <summary>
+        /// Gets a collection of paramters that configure the actions behaviour.
+        /// </summary>
+        public List<ConfigurationParameter> Configuration { get; set; }
+
+        /// <summary>
+        /// Sets-up the action ready for execution. 
+        /// </summary>
+        public void Setup()
+        {
+
+        }
+
+        /// <summary>
+        /// Executes the primary logic of the action.
+        /// </summary>
+        public void Execute()
+        {
+
+        }
+
+        /// <summary>
+        /// Tears-down the action after execution has completed.
+        /// </summary>
+        /// <remarks>For Example: freeing resources, closing connections and putting 3rd party devices, software or services into a good state.</remarks>
+        public void Teardown()
+        {
+
+        }
     }
 }
---
+++ src/Springhare/ConfigurationParameter.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) James C Dingle. All rights reserved.:warning
+
+using System;
+
+namespace Springhare
+{
+    /// <summary>
+    /// Represents a value that is used to configure how an action behaves at runtime.
+    /// </summary>
+    public class ConfigurationParameter
+    {
+        /// <summary>
+        /// Gets the name of the parameter.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets or sets the value of the parameter.
+        /// </summary>
+        public dynamic Value { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="ConfigurationParameter" /> class.
+        /// </summary>
+        public ConfigurationParameter(string name, dynamic value)
+        {
+            Name = string.IsNullOrWhiteSpace(name) ? name : throw new ArgumentNullException(nameof(name));
+            Value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+    }
+}
