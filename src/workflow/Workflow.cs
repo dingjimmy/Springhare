e4f5b18fa70938b286dd785f51afe0e6159c8d8a
James Dingle <dingjimmy@users.noreply.github.com>
2021-08-24 21:56:25 +0000

    Workflow first cut

--- src/workflow/Workflow.cs
+++ src/workflow/Workflow.cs
@@ -1,6 +1,46 @@
-﻿namespace Springhare.Workflow
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Springhare.Workflow
 {
     public class Workflow
     {
+        private TimeSpan _StepTimeout = new TimeSpan(0,0,0,1);
+
+        public IEnumerable<IStep> Steps { get; set; }
+
+        public async Task Start()
+        {
+
+            foreach (var step in Steps)
+            {
+                using (var cts = new CancellationTokenSource(_StepTimeout))
+                {
+                    await step.InitialiseAsync(cts.Token);
+                }
+            }
+
+            foreach (var step in Steps)
+            {
+                using (var cts = new CancellationTokenSource(_StepTimeout))
+                {
+                    await step.StartAsync(cts.Token);
+                }
+            }
+
+        }
+
+        public async Task Stop()
+        {
+            foreach (var step in Steps) 
+            {
+                using (var cts = new CancellationTokenSource(_StepTimeout))
+                {
+                    await step.AbortAsync(cts.Token);
+                }
+            }
+        }
     }
 }
